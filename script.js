e8293d8136da2c5d2282145371673add97366964
Elizabeth <ekkjohnson@gmail.com>
2022-03-23 17:46:59 -0500

    functions

---
+++ script.js
@@ -0,0 +1,79 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+function generatePassword(){
+return "password";
+}
+
+function indexRandomizer(){
+
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+//1. Create variables to save user input from prompts
+  //passwordLength string (read it to be integer)
+  //specialCharacter boolean
+  //numericCharacters boolean
+  //lowercaseCharacters boolean
+  //uppercaseCharacters boolean
+    //alert, confirm, prompt
+
+//2. Create arrays for each type of character
+  //specialCharacters, uppercase, lowercase, numeric
+
+  //4. use a randomizer function to select characters from each array
+
+//3. generatePassword function -- take user input and grab values from arrays and generate a password
+
+//5. create an empty array the represents the final password
+
+//6. join the final character arrays together based on user input
+
+//7. randomize characters from the joined arrays
+
+//8. transfomring the 
+
+Array.join("")
+
+var generateBtwn = document.querySelector("#generate")
+
+var lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var specChar =["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"]
+var digits = ["1", "2", "3", "4", "5","6","7","8","9"]
+
+var userselection= {
+  lower: true,
+  upper: true,
+  spec: true,
+  digits: true,
+}
+
+function writePassword(){
+  var password=generatePassword();
+  var passwordText = document.querySelector("#password");
+  
+  passwordText.value = password;
+}
+
+function generatePassword(){
+  return;
+  var charLength="Character length can't exceed 128 characters"
+  prompt
+}
+
+generateBtn.addEventListener("click", writePassword)
