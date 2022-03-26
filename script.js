db2a028407b3b2abc66049e48279f34ca44eca67
Elizabeth <ekkjohnson@gmail.com>
2022-03-26 13:49:45 -0500

    bug fix

--- script.js
+++ script.js
@@ -57,6 +57,7 @@ var specChar =["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", 
 var digits = ["0", "1", "2", "3", "4", "5","6","7","8","9"]
 
 var container=[]
+var newPassword= "";
 
 const charType=["lowCase", "upperCase", "specChar","digits"];
 
